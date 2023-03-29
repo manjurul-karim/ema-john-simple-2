7dc9e2117b843c50ec2e6c5fe9aed83b537248be
manjurul17002 <manjurul17002@gmail.com>
2023-03-29 23:46:12 +0600

    'cart'

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,14 +4,25 @@ import './Cart.css'
 const Cart = ({cart}) => {
     // const cart =props.cart //system-01
     // const {cart} = props
+
+    let totalPrice = 0 ;
+    let totalShipping = 0;
+    for (const product of cart){
+      totalPrice = totalPrice + product.price
+      totalShipping = totalShipping + product.shipping
+    }
+    const tax = totalPrice * 7 / 100 ;
+    const grandTotal = totalPrice + totalShipping + tax ;
     return (
       <div className='cart'>
         <h4>Oder summary</h4>
-        <p>Selected Items:{cart.length}</p>
-        <p>Total Price:</p>
-        <p>Total Shipping:</p>
-        <p>Tax:</p>
-        <h6>Grand Total:</h6>
+        <hr />
+        <p>Selected Items: {cart.length}</p>
+        <p>Total Price:   ${totalPrice}</p>
+        <p>Total Shipping: ${totalShipping}</p>
+        <p>Tax:  ${tax.toFixed(2)}</p>
+        <hr />
+        <h5>Grand Total:  ${grandTotal.toFixed(2)}</h5>
       </div>
     );
 };
