fc66ab57e63f32aff88a063d5ce17f23f91a22bd
manjurul17002 <manjurul17002@gmail.com>
2023-04-05 21:25:02 +0600

    shopping cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Product from "../../components/Product/Product";
 import Cart from "../Cart/Cart";
 import "./Shop.css";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -13,9 +14,15 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
 
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    console.log(storedCart);
+  },[])
+
     const handleAddToCart = (product) => {
       const newCart = [...cart, product]
       setCart(newCart)
+      addToDb(product.id)
     };
   return (
     <div className="shop-container">
